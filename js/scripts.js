1d34eabceff7b06df1a819c2295d5c86a2418b22
me <me@me.me.com>
2020-04-21 15:00:18 -0700

    finish up project

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,23 @@ $(document).ready(function(){
 
 
   $("form").submit(function(e){
+    e.preventDefault();
     var inputs = $("input").map(function(){
       return $(this).val();
-    }.get());
+    }).get();
+    console.log(inputs);
     var newArray = [];
     var one = inputs[0];
-    var two = input[1];
-    var three = input[2];
+    var two = inputs[1];
+    var three = inputs[2];
 
-    
-  
+    newArray.push(two);
+    newArray.push(three);
+    newArray.push(one);
+
+    newArray.forEach(function(item){
+      $("ul").append("<li>"+ item +"</li>")
+    });
   
   
   });
