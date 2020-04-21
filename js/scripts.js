84354b1dbfb752cc512322d03c6c0f3447edb681
me <me@me.me.com>
2020-04-21 14:52:53 -0700

    create form that gets inputs

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+
+  $("form").submit(function(e){
+    var inputs = $("input").map(function(){
+      return $(this).val();
+    }.get());
+    var newArray = [];
+    var one = inputs[0];
+    var two = input[1];
+    var three = input[2];
+
+    
+  
+  
+  
+  });
+
+
+});
